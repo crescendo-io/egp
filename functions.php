ec059903daa7a81e0501577ebb5073afb6c715c9
Bryan VIDAL <b.vidal@lonsdale.fr>
2024-06-11 20:43:11 +0200

    add post type galerie

--- functions.php
+++ functions.php
@@ -7,3 +7,65 @@ function wpm_enqueue_styles(){
 }
 
 
+function egp_custom_post_type() {
+    $labels = array(
+        'name'                => __( 'Galerie', 'lsd_lang'),
+        'singular_name'       => __( 'Galerie', 'lsd_lang'),
+        'menu_name'           => __( 'Galerie', 'lsd_lang'),
+        'all_items'           => __( 'Tous les types de Galerie', 'lsd_lang'),
+        'view_item'           => __( 'Voir tous les types de Galerie', 'lsd_lang'),
+        'add_new_item'        => __( 'Ajouter une Galerie', 'lsd_lang'),
+        'add_new'             => __( 'Ajouter', 'lsd_lang'),
+        'edit_item'           => __( 'Editer un type la Galerie', 'lsd_lang'),
+        'update_item'         => __( 'Modifier un type la galerie', 'lsd_lang'),
+        'not_found'           => __( 'Non trouvée', 'lsd_lang'),
+        'not_found_in_trash'  => __( 'Non trouvée dans la corbeille', 'lsd_lang'),
+    );
+
+    $args = array(
+        'label'               => __( 'Types de Galerie', 'lsd_lang'),
+        'description'         => __( 'Toutes les Galerie', 'lsd_lang'),
+        'labels'              => $labels,
+        'supports'            => array( 'title', 'author', 'revisions', 'custom-fields' ),
+        'show_in_rest'        => false,
+        'menu_icon'           => 'dashicons-admin-home',
+        'hierarchical'        => true,
+        'public'              => true,
+        'publicly_queryable' => true,
+        'has_archive'         => 'galerie',
+        'rewrite' => array(
+            'with_front' => true
+        )
+    );
+
+    register_post_type( 'galerie', $args );
+}
+
+add_action( 'init', 'egp_custom_post_type', 0 );
+
+
+
+function ecla_taxonomy() {
+    register_taxonomy(
+        'typo_client',
+        'galerie',
+        array(
+            'hierarchical' => true,
+            'show_admin_column' => true,
+            'label' => __( 'Typologie de client', 'lsd_lang'),
+            'query_var' => true
+        )
+    );
+
+    register_taxonomy(
+        'typo_product',
+        'galerie',
+        array(
+            'hierarchical' => true,
+            'show_admin_column' => true,
+            'label' => __( 'Typologie de produit', 'lsd_lang'),
+            'query_var' => true
+        )
+    );
+}
+add_action( 'init', 'ecla_taxonomy');
