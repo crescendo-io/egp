5d4015edaef4721b7d5c6418856dcac6d23b1dca
Bryan VIDAL <b.vidal@lonsdale.fr>
2024-07-07 13:23:37 +0200

    suite integration

--- template-parts/general/fonts.php
+++ template-parts/general/fonts.php
@@ -1,3 +1,57 @@
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+<style type="text/css">
+    @font-face {
+        font-family: 'Readex Pro';
+        src: url('<?= get_stylesheet_directory_uri();?>/fonts/ReadexPro-Medium.woff2') format('woff2'),
+        url('<?= get_stylesheet_directory_uri();?>/fonts/ReadexPro-Medium.woff') format('woff');
+        font-weight: 500;
+        font-style: normal;
+        font-display: swap;
+    }
+
+    @font-face {
+        font-family: 'Readex Pro';
+        src: url('<?= get_stylesheet_directory_uri();?>/fonts/ReadexPro-Bold.woff2') format('woff2'),
+        url('<?= get_stylesheet_directory_uri();?>/fonts/ReadexPro-Bold.woff') format('woff');
+        font-weight: bold;
+        font-style: normal;
+        font-display: swap;
+    }
+
+    @font-face {
+        font-family: 'Readex Pro';
+        src: url('<?= get_stylesheet_directory_uri();?>/fonts/ReadexPro-ExtraLight.woff2') format('woff2'),
+        url('<?= get_stylesheet_directory_uri();?>/fonts/ReadexPro-ExtraLight.woff') format('woff');
+        font-weight: 200;
+        font-style: normal;
+        font-display: swap;
+    }
+
+    @font-face {
+        font-family: 'Readex Pro';
+        src: url('<?= get_stylesheet_directory_uri();?>/fonts/ReadexPro-Light.woff2') format('woff2'),
+        url('<?= get_stylesheet_directory_uri();?>/fonts/ReadexPro-Light.woff') format('woff');
+        font-weight: 300;
+        font-style: normal;
+        font-display: swap;
+    }
+
+    @font-face {
+        font-family: 'Readex Pro';
+        src: url('<?= get_stylesheet_directory_uri();?>/fonts/ReadexPro-Regular.woff2') format('woff2'),
+        url('<?= get_stylesheet_directory_uri();?>/fonts/ReadexPro-Regular.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+        font-display: swap;
+    }
+
+    @font-face {
+        font-family: 'Readex Pro';
+        src: url('<?= get_stylesheet_directory_uri();?>/fonts/ReadexPro-SemiBold.woff2') format('woff2'),
+        url('<?= get_stylesheet_directory_uri();?>/fonts/ReadexPro-SemiBold.woff') format('woff');
+        font-weight: 600;
+        font-style: normal;
+        font-display: swap;
+    }
+
+
+</style>
