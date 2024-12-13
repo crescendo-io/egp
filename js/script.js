867e748b804a5d1e927ef0a0add589c3e39cd826
Bryan VIDAL <b.vidal@lonsdale.fr>
2024-12-13 13:07:23 +0100

    add pattern / debug mobile

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 $(window).on('load',function(){
     $('.burger-menu').click(function(){
        $('.main-menu').slideToggle(100);
+        $('html, body').animate({ scrollTop: 0 }, 'slow');
+        $('html, body').toggleClass('open');
     });
+
 
     $('.arrow-sub').click(function(){
         var el = $(this);
