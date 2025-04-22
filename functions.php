47865b436f34643393ed20f19d42ea492c6bf069
Bryan VIDAL <b.vidal@lonsdale.fr>
2025-04-22 18:11:00 +0200

    clear cache

--- functions.php
+++ functions.php
@@ -32,7 +32,8 @@ add_action( 'wp_enqueue_scripts', 'wpm_enqueue_styles' );
 function wpm_enqueue_styles(){
     //wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/styles/theme.css' );
     wp_enqueue_style('lightbox', get_stylesheet_directory_uri() . '/styles/lightbox.css', array(), filemtime(get_template_directory() . '/styles/theme.css'));
-    wp_enqueue_style('theme', get_stylesheet_directory_uri() . '/styles/theme.css?cache=34983987', array(), filemtime(get_template_directory() . '/styles/theme.css'));
+    wp_enqueue_style('theme', get_stylesheet_directory_uri() . '/styles/theme.css?cache=349839879
+    ', array(), filemtime(get_template_directory() . '/styles/theme.css'));
     wp_enqueue_script(
         'lightbox', // Identifiant unique du script
         get_stylesheet_directory_uri() . '/js/lightbox.min.js', // URL du fichier JS
