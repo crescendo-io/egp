b96fcaedfb5e29d15d703a6aabf1b52b39308bbc
Bryan VIDAL <b.vidal@lonsdale.fr>
2025-05-26 17:53:40 +0200

    rm doublon GTM

--- header.php
+++ header.php
@@ -81,15 +81,7 @@
 </head>
 
 <body <?php body_class(); ?> data-module="bugReport">
-<?php if($searchConsent): ?>
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-5TM5LDG');</script>
-    <!-- End Google Tag Manager -->
-<?php endif; ?>
+
 
 <?php
     $optionMenu = get_field('option_menu_style', 'option');
